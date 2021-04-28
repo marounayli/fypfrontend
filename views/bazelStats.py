60139d6e178150830003d55facf4a14d6ce21a99
Roland Salloum <roland.salloum00@outlook.com>
2021-04-28 22:40:14 +0200

    feat: structure for compare graph

--- views/bazelStats.py
+++ views/bazelStats.py
@@ -4,10 +4,10 @@ import pandas as pd
 import plotly.express as px
 import requests
 
-request_url = "http://localhost:8081/bazel-stats/agg"
+agg_request_url = "http://localhost:8081/bazel-stats/agg"
 aggregationSize = 2
 aggregationType = [
-    "sum", "min"
+    "sum", "min", "max", "min"
 ]
 body = {
     "aggregationSize": aggregationSize,
@@ -15,36 +15,59 @@ body = {
 }
 headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
 
-def fetch_data():
-    aggregation_request = requests.post(request_url, json=body, headers=headers)
+
+def fetch_data_aggregation(request):
+    aggregation_request = requests.post(request, json=body, headers=headers)
+    json_data = aggregation_request.json()
+    return json_data
+
+
+def fetch_data_last_N_bazel_stats(request):
+    aggregation_request = requests.get(request)
     json_data = aggregation_request.json()
     return json_data
 
 
 def parse_data(json_data):
-    res=dict()
+    res = dict()
     for key in json_data.keys():
-        res[key]=dict()
-    print(res)
+        res[key] = dict()
     for key in json_data.keys():
         for obj in json_data[key]["payload"]:
-            res[key][obj["name"]]=obj["time"]
+            res[key][obj["name"]] = obj["time"]
     return res
 
-fig = px.bar(pd.DataFrame.from_dict(parse_data(fetch_data())), barmode="group")
 
-bazel_stats_layout= html.Div(children=[
+def compare_data(json_data):
+    json_data2 = fetch_data_last_N_bazel_stats("http://localhost:8081/bazel-stats/2")
 
-        html.H1(children='Bazel Stats'),
+    print(json_data2)
+    return parse_data(json_data)
 
-        html.Div(children='''
-        Dash: A web application framework for Python.
+
+fig = px.bar(pd.DataFrame.from_dict(parse_data(fetch_data_aggregation(agg_request_url))), barmode="group")
+
+fig2 = px.bar(pd.DataFrame.from_dict(compare_data(fetch_data_aggregation(agg_request_url))), barmode="group")
+
+bazel_stats_layout = html.Div(children=[
+
+    html.H1(children='Bazel Stats'),
+
+    html.Div(children='''
+        A Graph That Represents The Aggregation Of The Last N Bazel Builds.
     '''),
 
-        dcc.Graph(
-            id='example-graph',
+    dcc.Graph(
+        id='Aggregation Graph',
+        figure=fig
+    ),
 
-            figure=fig
-        )
-    ])
+    html.Div(children='''
+    A Graph That Compares 2 Chosen Builds.
+    '''),
 
+    dcc.Graph(
+        id='Comparator Graph',
+        figure=fig2
+    )
+])
